3b3e033834d9316e20c915521a7a69e76c7ca406
Artur Gevorgyan <artur93gev@gmail.com>
2019-07-12 15:43:29 +0400

    script initial state

---
+++ README.md
@@ -0,0 +1,30 @@
+# App-loader
+
+Script can be usefull if you want to manipulate with repositories. 
+
+## Usage example
+
+```js
+// config.js
+const loader = require('app-loader');
+
+const configs = {
+  app: {
+    path: 'some_local_path',
+    repositoryUrl: 'some_git_repository_url',
+    branch: 'prod_branch_name',
+    devBranch: 'dev_branch_name',
+  },
+  app1: {
+    path: 'some_local_path1',
+    repositoryUrl: 'some_git_repository_url',
+    branch: 'prod_branch_name',
+    devBranch: 'dev_branch_name',
+  },
+};
+
+loader(configs);
+```
+
+Than in you can call it from command line executing `node config.js`.
+The script will clone the repository either from `devBranch` or from `branch` depending on `process.env.NODE_ENV`(default master).
---
+++ index.js
@@ -0,0 +1,38 @@
+const git = require('simple-git')(process.env.PWD);
+const exec = require('child_process').exec;
+
+const loader = configs => {
+  const isDev = process.env.NODE_ENV === 'development';
+
+  const syncIterate = (apps, index) => {
+    const app = apps[index];
+    const { branch, repositoryUrl, branchDev, path } = configs[app];
+    const usedBranch = isDev ? branchDev || branch : branch || 'master';
+
+    console.log(`Removing directory at ${path} path`);
+    exec(`rm -rf ${path}`, () => {
+  
+      console.log(`Starting to load ${app} ...`);
+      git.clone(repositoryUrl, path, `-b${usedBranch}`, err => {
+        if (!err) {
+
+          console.log(`Application ${app} loaded successfully from ${usedBranch} branch of ${repositoryUrl} remote path.`);
+          exec(`cd ${path} && npm i && cd -`, (err, stdout, stderr) => {
+            if (apps.length - 1 > index) {
+              syncIterate(apps, index + 1);
+            } else {
+              console.log('Application is ready to start!!!');
+            }
+          });
+        }
+      });
+    });
+  };
+
+  const apps = Object.keys(configs);
+  const index = 0;
+  
+  syncIterate(apps, index);
+}
+
+module.exports = loader;
